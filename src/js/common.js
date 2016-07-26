7bbeedc4efd8ffc9ccd8ec32ee18e0ebb315f1be
wanghaifei <654779598@qq.com>
2016-07-26 16:51:25 +0800

    表单验证完成

--- src/js/common.js
+++ src/js/common.js
@@ -30,3 +30,84 @@ $(function(){
 		});
 	});
 });
+
+/*----------表单验证-----------*/
+$(function(){
+	var $txt = $("#logTxt");
+	var $psw = $("#logpsw");
+	var $zhuCe = $(".zhuCe");
+	var $nextpsw = $("#nextpsw");
+
+	var txtTrue = true;
+	var pswTrue = true;
+	var nextpsw = true;
+
+	$("<span/>").addClass("yzTxt").insertAfter($zhuCe);
+	$("<span/>").addClass("yzPsw").insertAfter($zhuCe);
+	$("<span/>").addClass("nextpsw").insertAfter($zhuCe);
+	//帐号验证
+	$txt.on("fous",function(){
+		$(".yzTxt").css("zIndex",20);
+	});
+
+
+	$txt.on("blur",function(){
+		var regphone = /(^1[0-9]{10}$)|(^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$)/;
+
+		var txtval = $txt.val();
+		
+		if(txtval==""){
+			$(".yzTxt").html("请输入常用手机号或邮箱");
+			txtTrue = false;
+		}
+		else if(!regphone.test(txtval)){
+			$(".yzTxt").html("请输入正确的常用手机号或邮箱");
+			txtTrue = false;
+		}
+		else{
+			$(".yzTxt").html("");
+			txtTrue = true;
+			$(".yzTxt").css("zIndex",0);
+		}
+	});
+	//密码验证
+	$psw.on("fous",function(){
+		$(".yzPsw").css("zIndex",20);
+	});
+	
+	$psw.on("blur",function(){
+		
+		var regPsw = /^[a-zA-z0-9]\w{3,15}$/;
+		var pswval = $psw.val();
+
+		if(pswval==" "){
+			$(".yzPsw").html("请输入密码");
+			pswTrue = false;
+		}
+		else if(!regPsw.test(pswval)){
+			$(".yzPsw").html("密码要在4到16之间哦");
+			pswTrue = false;
+		}
+		else{
+			$(".yzPsw").html("");
+			$(".yzPsw").css("zIndex",0);
+		};
+	});
+
+	$nextpsw.on("fous",function(){
+		$(".nextpsw").css("zIndex",20);
+	});
+
+
+	$nextpsw.on("blur",function(){
+		if($nextpsw.val() != $psw.val()){
+			$(".nextpsw").html("两次密码不一致");
+			nextpsw = false;
+		}
+		else{
+			$(".nextpsw").html("");
+			nextpsw = true;
+			$(".nextpsw").css("zIndex",0);
+		}
+	});
+});
--- src/js/index.js
+++ src/js/index.js
@@ -19,66 +19,69 @@
 		});
 	});	
 	//---------------------------banner-------
-			$(function(){
-				var timer;
-				var $banner = $("#ban-pic");
-				var $movePic = $banner.find('ul');
-				
-				var $picLeft = $('.ban-picLeft');
-				var $picRight = $('.ban-picRight');
+$(function(){
+	var timer;
+	var $banner = $("#ban-pic");
+	var $movePic = $banner.find('ul');
 	
-				var index = 0;
-				var speed = 960;
-				timer = setInterval(init,1000);
-				init();
-	
-				function init(){
-					index++;
-					if(index>5){
-						$movePic.css('left',0);
-						index = 1;
-					};
-					show();
-				}
-				function show(){
-					$movePic.stop().animate({
-						left : -index*speed
-					});
-				};
+	var $picLeft = $('.ban-picLeft');
+	var $picRight = $('.ban-picRight');
 
-				function move(){
-					index--;
-					if(index<0){
-						index = 4;
-						$movePic.css('left',-4740);
-					};
-					show();
-				};
-	
-	
-				$banner.hover(function(){
-					clearInterval(timer);
-					$picLeft.show();
-					$picRight.show();
-				},function(){
-					timer = setInterval(init,4000);
-					$picLeft.hide();
-					$picRight.hide();
-				});
-	
-				$picLeft.on('click',function(){
-					move();
-				}).hover(function(){
-					$(this).css('background',"url('css/img/jt.png')")
-				},function(){
-					$(this).css('background',"url('css/img/jt.png') 0 -201px")
-				});
-	
-				$picRight.on('click',function(){
-					init();
-				}).hover(function(){
-					$(this).css('background',"url('css/img/jt.png') 0 -66px")
-				},function(){
-					$(this).css('background',"url('css/img/jt.png') 0 -134px")
-				});
-			});
+	var index = 0;
+	var speed = 960;
+	timer = setInterval(init,1000);
+	init();
+
+	function init(){
+		index++;
+		if(index>5){
+			$movePic.css('left',0);
+			index = 1;
+		};
+		show();
+	}
+	function show(){
+		$movePic.stop().animate({
+			left : -index*speed
+		});
+	};
+
+	function move(){
+		index--;
+		if(index<0){
+			index = 4;
+			$movePic.css('left',-4740);
+		};
+		show();
+	};
+
+
+	$banner.hover(function(){
+		clearInterval(timer);
+		$picLeft.show();
+		$picRight.show();
+	},function(){
+		timer = setInterval(init,4000);
+		$picLeft.hide();
+		$picRight.hide();
+	});
+
+	$picLeft.on('click',function(){
+		move();
+	}).hover(function(){
+		$(this).css('background',"url('css/img/jt.png')")
+	},function(){
+		$(this).css('background',"url('css/img/jt.png') 0 -201px")
+	});
+
+	$picRight.on('click',function(){
+		init();
+	}).hover(function(){
+		$(this).css('background',"url('css/img/jt.png') 0 -66px")
+	},function(){
+		$(this).css('background',"url('css/img/jt.png') 0 -134px")
+	});
+});
+
+
+//---------------------------sold-code-------
