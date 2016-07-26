9ee2175f9eed74df8fb9f5a807200f4c4ab13c80
whf <654779598@qq.com>
2016-07-27 00:42:17 +0800

    完善首页，购物车

--- src/js/shopping-cart.js
+++ src/js/shopping-cart.js
@@ -8,18 +8,35 @@ $(function(){
 	var $corRight = $('.corRight');
 
 	var spe= 960;
-	timer = setInterval(thingShow,2000);
+	speInit();
+	timer = setInterval(thingShow,5000);
 	thingShow();
 
+
+	function speInit(){
+		for(var i=0;i<$thing.length;i++){
+			$($thing.eq(i)).css("left",i*spe);
+		}
+	};
 	function thingShow(){
-		$.each($thing,function(idx,item){
+		/*$.each($thing,function(idx,item){
 			var zero = parseInt($(item).css("left"));
 			$(item).animate({left: zero-spe},function(){
 				if(parseInt($(this).css("left"))<0){
-					$(item).css("left",-spe);
+					$(item).css("left",spe);
 				}
-			})
-		});
+			});
+			$(item).next("div").animate({left : zero});
+		});*/
+		for(var i=0;i<$thing.length;i++){
+			var zero = parseInt($thing.eq(i).css("left"));
+			$thing.eq(i).stop().animate({left: zero-spe,zIndex:10},function(){
+				if(parseInt($(this).css("left"))<-spe){
+					$(this).css({left:spe,zIndex:10});
+				}
+			});
+			$thing.eq(i+1).stop().animate({left : zero});
+		};
 	};
 
 });
